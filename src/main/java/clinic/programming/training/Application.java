c1cfe19cf1af684623dad781a997c2cb54f2a5f6
Prasanna <kbnpkecm@gmail.com>
2018-01-27 13:00:43 +0530

    Adding Application Java Source code

---
+++ src/main/java/clinic/programming/training/Application.java
@@ -0,0 +1,14 @@
+ public class Application {
+    
+	 public Application()
+	 {
+         System.out.println ("Inside Application");
+     }
+      
+   public static void main (String[] args) 
+   {
+	  System.out.println ("Starting Application");
+	  Application app = new Application();
+      
+   }
+ }
